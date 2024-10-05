f92101fec1cb4e5cac58cc01b5173a181f0780a2
José Paz <138523584+devJosePaz@users.noreply.github.com>
2024-10-05 14:02:24 +0000

    add primeira pagian PHP

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php
+
+     $num1 = 60;
+     $num2 = 9;
+
+     $soma = $num1 + $num2;
+
+    ?>
+
+    <p>A soma de 60 e 9 é: <?php echo $soma ?> </p>
+    
+</body>
+</html>
